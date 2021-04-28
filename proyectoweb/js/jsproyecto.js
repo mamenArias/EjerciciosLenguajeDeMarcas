d25ffbfaf7de28126608e5c38c3a7623098772a7
Mª Carmen Arias <mcariasdeharo@gmail.com>
2021-04-28 20:22:46 +0200

    Imágenes
    Las imágenes se amplian ocupando la pantalla al hacer click sobre ellas.

--- proyectoweb/js/jsproyecto.js
+++ proyectoweb/js/jsproyecto.js
@@ -21,6 +21,7 @@ function main() {
     });
 }
 
+//Botón del menú lateral
 var dropdown = document.getElementsByClassName("botonDesplegable");
 var i;
 
@@ -35,3 +36,18 @@ for (i = 0; i < dropdown.length; i++) {
         }
     });
 }
+
+//Animación de las imágenes para abrirse a pantalla completa cuando hacemos click
+$(document).ready(function() {
+
+    //Al hacer clic en cualquier img ejecutamos la acción
+    $('.content img').click(function() {
+        //Capturamos el src de la img
+        var postimg = $(this).attr('src');
+        //Agregamos el src a una href simbólico
+        $('#verimagenes').attr('href', postimg);
+        //Hacemos clic en el enlace para activar el visor
+        $('#verimagenes').click();
+    });
+
+});
