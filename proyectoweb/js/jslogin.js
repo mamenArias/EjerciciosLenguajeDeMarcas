4d2bbb711d25bff1d88ddcd46a9f44e799d76afd
Mª Carmen Arias <mcariasdeharo@gmail.com>
2021-05-14 13:24:43 +0200

    Revert "prueba2"
    This reverts commit 0a4f1878d2c541a75026bf90097604d2446be784.

--- proyectoweb/js/jslogin.js
+++ proyectoweb/js/jslogin.js
@@ -1,17 +1,17 @@
 window.onload = function comprobarLogin() {
-    if (localStorage.getItem("user").length > 0) {
-        $('#loginBoton').css('display', 'none');
+    if (sessionStorage.getItem("user").length > 0) {
+        $('#loginOculto').css('display', 'none');
         $('#logout').css('display', 'inline-block');
         $('#conectado').css('display', 'inline-block');
     }
 }
-document.getElementById("conectado").innerHTML = "| Bienvenid@ " + localStorage.getItem("user") + " |";
+document.getElementById("conectado").innerHTML = "| Bienvenid@ " + sessionStorage.getItem("user") + " |";
 
 
 function cerrarSesion() {
     if (typeof(Storage) !== "undefined") {
         if (confirm("¿Desea Cerrar Sesión?")) {
-            localStorage.removeItem("user");
+            sessionStorage.removeItem("user");
             location.reload();
         } else {
 
@@ -57,8 +57,8 @@ function miFuncion(xml) {
                 pass = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
                 if (typeof(Storage) !== 'undefined') {
                     // Código cuando Storage es compatible
-                    localStorage.setItem('user', nombre);
-                    localStorage.setItem('clave', pass);
+                    sessionStorage.setItem('user', nombre);
+                    sessionStorage.setItem('clave', pass);
                 } else {
                     alert("El navegador no es compatible con SessionStorage.")
                         // Código cuando Storage NO es compatible
