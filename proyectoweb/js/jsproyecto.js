5aab9ffdd17263b190c720e7ebc72c1f6d23ae63
Mª Carmen Arias <mcariasdeharo@gmail.com>
2021-05-15 14:38:03 +0200

    borrar login

--- proyectoweb/js/jsproyecto.js
+++ proyectoweb/js/jsproyecto.js
@@ -51,27 +51,6 @@ $(document).ready(function() {
 });
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-document.getElementById("conectado").innerHTML = "| Bienvenid@ " + sessionStorage.getItem("user") + " |";
->>>>>>> parent of 0a4f187 (prueba2)
-=======
-document.getElementById("conectado").innerHTML = "| Bienvenid@ " + sessionStorage.getItem("user") + " |";
->>>>>>> parent of 0a4f187 (prueba2)
-//Código para leer el XML y validar el usuario y contraseña
-function leerXML() {
-    // lee desde aquí.
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            miFuncion(this);
-        }
-    };
-    xhr.open("GET", "registrados.xml", true);
-    xhr.send();
-}
-
 
 // Ocultamos el botón de desconectar
 /*function ocultar() {
@@ -81,131 +60,7 @@ function leerXML() {
 <<<<<<< HEAD
 }*/
 
-//document.getElementById("conectado").innerHTML = "| Bienvenid@ " + sessionStorage.getItem("user") + " |";
-=======
-}
 
-//document.getElementById("conectado").innerHTML = "| Bienvenid@ " + sessionStorage.getItem("user") + " |";
-
-function cerrarSesion() {
-    if (typeof(Storage) !== "undefined") {
-        if (confirm("¿Desea Cerrar Sesión?")) {
-            sessionStorage.removeItem("user");
-            location.reload();
-        } else {
-
-        }
-
-    } else {
-        alert("Este navegador no soporta web storage...");
-    }
-
-    return false;
-}
->>>>>>> parent of 0a4f187 (prueba2)
-
-function miFuncion(xml) {
-
-    var i;
-    var xmlDoc = xml.responseXML;
-    var x = xmlDoc.getElementsByTagName("usuario");
-    var loginCorrecto = false;
-    var nombreUsuario = document.getElementById("user").value;
-    var passUsuario = document.getElementById("pass").value;
-
-    for (i = 0; i < x.length; i++) {
-
-<<<<<<< HEAD
-        if (x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue == nombreUsuario) {
-=======
-        if (x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue == document.getElementById("user").value &&
-            x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue == document.getElementById("pass").value) {
-            loginCorrecto = true;
-            nombre = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-            clave = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
-
-            if (typeof(Storage) != 'undefined') {
-                sessionStorage.setItem('user', nombre);
-                sessionStorage.setItem('pass', clave);
-            } else {
-                alert("El navegador no es compatible con SessionStorage.")
-            }
->>>>>>> parent of 0a4f187 (prueba2)
-
-            if (x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue == passUsuario) {
-
-                loginCorrecto = true;
-                //nombre = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-                //clave = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
-
-                if (typeof(Storage) !== 'undefined') {
-
-                    sessionStorage.setItem('user', nombreUsuario);
-
-                    sessionStorage.setItem('pass', passUsuario);
-
-                    break;
-                } else {
-                    alert("El navegador no es compatible con SessionStorage.")
-                }
-            }
-        }
-    }
-
-    if (loginCorrecto == true) {
-
-        alert("Bienvenid@ " + nombreUsuario);
-        $(".logout").show();
-        $(".loginBoton").hide();
-        $(".login").hide();
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 0a4f187 (prueba2)
-        document.getElementById("conectado").innerHTML = "Bienvenid@ " + sessionStorage.getItem("user");
-        $("#conectado").show();
->>>>>>> parent of 0a4f187 (prueba2)
-        $(".textoOculto").show();
-        $("#conectado").value = nombreUsuario;
-        $("#conectado").show();
-        //document.getElementById('#loginBoton').style.display = "none";
-        //document.getElementById('#conectado').value = nombreUsuario;
-        //document.getElementById('#conectado').style.display = "block";
-        //location.reload();
-    } else {
-        alert("Usuario o contraseña incorrectos.");
-    }
-
-
-}
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-function cerrarSesion() {
-    if (typeof(Storage) !== "undefined") {
-        if (confirm("¿Desea Cerrar Sesión?")) {
-            sessionStorage.removeItem("user");
-            sessionStorage.removeItem("pass");
-            location.reload();
-        }
-        return false;
-    }
-}
-
-function comprobarLogin() {
-    if (sessionStorage.getItem('user').length > 0) {
-        document.getElementById('#loginBoton').style.display = "none";
-        document.getElementById('#conectado').value = nombreUsuario;
-        document.getElementById('#conectado').style.display = "block";
-
-        document.getElementById('#logout').style.display = "inline-block";
-        //  document.getElementById('.conectado').innerHTML = sessionStorage.getItem("usuario");
-    } else {
-        document.getElementById('#logout').style.display = "none";
-=======
-=======
->>>>>>> parent of 0a4f187 (prueba2)
 /*function comprobarLogin() {
 
     if (sessionStorage.getItem('user').length > 0) {
@@ -218,4 +73,4 @@ function comprobarLogin() {
 
 window.onload() = function launch() {
     comprobarLogin();
-}
+}*/
