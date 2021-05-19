b06ba5aa260d44ecb1786d37e2c45df329f49520
Mª Carmen Arias <mcariasdeharo@gmail.com>
2021-05-19 10:22:17 +0200

    Modo noche

--- proyectoweb/js/jsproyecto.js
+++ proyectoweb/js/jsproyecto.js
@@ -50,7 +50,12 @@ $(document).ready(function() {
 
 });
 
+const switchButton = document.getElementById('switch');
 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active'); //toggle the HTML button with the id='switch' with the class 'active'
+});
 
 // Ocultamos el botón de desconectar
 /*function ocultar() {
